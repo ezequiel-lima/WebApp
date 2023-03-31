bf3327e2b54bbc63469cd2062d650e55d8b8b2ee
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2023-03-31 19:51:10 -0300

    Ajustes Modificacoes

---
+++ WebApp/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+	public class ProdutoController : Controller
+	{
+		public IActionResult CadastrarProduto()
+		{
+			return View();
+		}
+	}
+}
